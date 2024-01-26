0405ff950131b0deb9615b1daf484fdd4c693e37
Sudhir Das <sudhirdas1999@gmail.com>
2024-01-26 16:09:47 +0530

    added employee repository

--- src/main/java/com/serius/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/serius/employeemanager/repo/EmployeeRepo.java
@@ -3,6 +3,11 @@ package com.serius.employeemanager.repo;
 import com.serius.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
