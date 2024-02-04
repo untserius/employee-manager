1d9764f11b57b06eba13e2e2fec7ede90c609679
Sudhir Das <sudhirdas1999@gmail.com>
2024-02-04 15:45:05 +0530

    UI Modal Tested

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
-import { NgFor, NgForOf } from '@angular/common';
+import { NgFor, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, NgFor],
+  imports: [RouterOutlet, NgForOf, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -31,5 +31,31 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    if (!container) {
+      console.error('Container element not found.');
+      return;
+    }
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container.appendChild(button);
+    button.click();
+    console.log('Method called');
+    console.log('Employee:', employee);
+    console.log('Mode:', mode);
   }
 }
