7af037e0b514806a672c285e3f83e4c31074a4e0
Sudhir Das <sudhirdas1999@gmail.com>
2024-05-07 18:10:22 +0530

    fixed CORS v2

---
+++ src/main/java/com/serius/employeemanager/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.serius.employeemanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://example.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "X-Auth-Token", "Authorization")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
